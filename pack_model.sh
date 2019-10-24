1d78609d80648f6907b37672d601e79de00aae2f
DanAnastasyev <dan.anastasyev@gmail.com>
2019-10-24 12:02:53 +0300

    Updated pack_model.sh

--- pack_model.sh
+++ pack_model.sh
@@ -1,3 +1,3 @@
 # bash
 
-zip -x "*__pycache__*" -r solution.zip data models solvers Dockerfile metadata.json solution.py utils.py
+zip -x "*__pycache__*" -r solution.zip data models solvers public_set Dockerfile metadata.json solution.py utils.py
