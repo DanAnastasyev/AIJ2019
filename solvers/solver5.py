a5d30d68e9db612fb1c1ac6c1adef8fe4f04c2bb
Nikolay Zinov <nzinov@yandex-team.ru>
2019-11-01 19:20:16 +0300

    fix

--- solvers/solver5.py
+++ solvers/solver5.py
@@ -13,21 +13,13 @@ class Solver(BertEmbedder):
     def __init__(self, seed=42):
         super(Solver, self).__init__()
         self.morph = pymorphy2.MorphAnalyzer()
-        self.ud_model = Model.load("data/udpipe_syntagrus.model".encode())
-        self.process_pipeline = Pipeline(self.ud_model, 'tokenize', Pipeline.DEFAULT, Pipeline.DEFAULT, 'conllu')
         self.seed = seed
         self.init_seed()
         self.toktok = ToktokTokenizer()
         self.paronyms = self.get_paronyms()
-        self.freq_bigrams = self.open_freq_grams()
 
     def init_seed(self):
         return random.seed(self.seed)
-
-    def open_freq_grams(selfself):
-        with open('data/bigrams_lemmas.pickle', 'rb') as inputfile:
-            counts = pickle.load(inputfile)
-        return counts
 
     def get_paronyms(self):
         paronyms = []
@@ -49,12 +41,12 @@ class Solver(BertEmbedder):
             paronyms.add(par2)
         try:
             closest = get_close_matches(par, list(paronyms))[0]
-        except IndexError:
-            closest = None
+        except indexerror:
+            closest = none
         return closest
 
     def check_pair(self, token_norm):
-        paronym = None
+        paronym = none
         for p1, p2 in self.paronyms:
             if token_norm == p1:
                 paronym = p2
@@ -69,81 +61,25 @@ class Solver(BertEmbedder):
         token_norm = token_all.normal_form
         paronym = self.check_pair(token_norm)
 
-        if paronym is None:
+        if paronym is none:
             paronym_close = self.find_closest_paronym(token_norm)
             paronym = self.check_pair(paronym_close)
 
-        if paronym is not None:
+        if paronym is not none:
             paronym_parse = self.morph.parse(paronym)[0]
             try:
                 str_grammar = str(token_all.tag).split()[1]
-            except IndexError:
+            except indexerror:
                 str_grammar = str(token_all.tag)
 
-            gr = set(str_grammar.replace("Qual ", "").replace(' ',',').split(','))
+            gr = set(str_grammar.replace("qual ", "").replace(' ',',').split(','))
             try:
                 final_paronym = paronym_parse.inflect(gr).word
-            except AttributeError:
+            except attributeerror:
                 final_paronym = paronym
         else:
             final_paronym = ''
         return final_paronym
-
-    def syntax_parse(self, some_text, token):
-        processed = self.process_pipeline.process(some_text.lower())
-        content = [l for l in processed.split('\n') if not l.startswith('#')]
-        tagged = [w.split('\t') for w in content if w]
-
-        linked_word = ''
-        for analysis in tagged:
-            if analysis[1] == token:
-                head = analysis[6]
-                if head == '0':
-                    root_id = analysis[0]
-                    for analysis in tagged:
-                        if analysis[6] == root_id:
-                            linked_word = analysis[1]
-                            break
-                else:
-                    for analysis in tagged:
-                        if analysis[0] == head:
-                            linked_word = analysis[1]
-                            break
-        return linked_word
-
-    def check_frequencies(self, sentences):
-        examples = []
-        for token, second_tok, line in sentences:
-            token = token.lower().rstrip('.,;:!?')
-            token_lemma = self.lemmatize(token)
-            second_lemma = self.lemmatize(second_tok)
-
-            collocation_word = self.syntax_parse(line, token)
-            collocation_lemma = self.lemmatize(collocation_word)
-
-            first = (collocation_lemma, token_lemma)
-            second = (collocation_lemma, second_lemma)
-
-            freq1 = self.freq_bigrams[first]
-            freq2 = self.freq_bigrams[second]
-
-            first = (token_lemma, collocation_lemma)
-            second = (second_lemma, collocation_lemma)
-            freq3 = self.freq_bigrams[first]
-            freq4 = self.freq_bigrams[second]
-
-            freq_first = freq1 + freq3
-            freq_second = freq2 + freq4
-
-            if freq_second > freq_first:
-                return second_tok
-            if freq_first == freq_second:
-                examples.append((0, freq_first, freq_second, token, second_tok))
-
-        good_paronym = ''
-        if examples:
-            good_paronym = examples[0][4]
-        return good_paronym
 
     def predict(self, task):
         return self.predict_from_model(task)
